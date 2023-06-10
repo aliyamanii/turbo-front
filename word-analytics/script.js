43902c398ca30f8d7a8a3dae3c49147a96e646c1
Ali Yamani <aliyamanii@outlook.com>
2023-06-10 22:44:44 +0330

    Added the Word Analytics files

---
+++ word-analytics/script.js
@@ -0,0 +1,39 @@
+const textAreaEl = document.querySelector('.textarea');
+const charStatEl = document.querySelector('.stat__number--characters');
+const wordStatEl = document.querySelector('.stat__number--words');
+const twitterStatEl = document.querySelector('.stat__number--twitter');
+const facebookStatEl = document.querySelector('.stat__number--facebook');
+
+textAreaEl.addEventListener('input', function() {
+    //input validation
+    if (textAreaEl.value.includes('<script>')){
+        alert('Oi cut that out!');
+        textAreaEl.value = 'Skibidi ba um dada'
+    }
+    //determine new numbers
+    let wordCount = textAreaEl.value.split(' ').length;
+    if (textAreaEl.value.length === 0) {
+        wordCount = 0;
+    }
+    const charCount = textAreaEl.value.length;
+    const twitterCount = 280 - charCount;
+    const facebookCount = 2200 - charCount;
+
+    //visual indicator that the limit is exceeded
+    if (twitterCount < 0) {
+        twitterStatEl.classList.add('stat__number--limit');
+    } else {
+        twitterStatEl.classList.remove('stat__number--limit');
+    }
+    if (facebookCount < 0) {
+        facebookStatEl.classList.add('stat__number--limit');
+    } else {
+        facebookStatEl.classList.remove('stat__number--limit');
+    }
+
+    //set new numbers
+    wordStatEl.textContent = wordCount;
+    charStatEl.textContent = charCount;
+    twitterStatEl.textContent = twitterCount;
+    facebookStatEl.textContent = facebookCount;
+})
